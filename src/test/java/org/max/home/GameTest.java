5c347c46e4dceb034b6448c71c153a280d73e13e
alenapech <alenaryzhova@inbox.ru>
2024-02-03 15:39:20 +0600

    hm1

--- src/test/java/org/max/home/GameTest.java
+++ src/test/java/org/max/home/GameTest.java
@@ -1,68 +1,58 @@
 package org.max.home;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Тест кейс для тестирования методов порадокса
  */
 public class GameTest {
 
-    static List<Door> doors;
+    private static List<Door> doors;
 
     @BeforeEach
-    void initDoors () {
+    void setUp() {
         doors = new ArrayList<>();
         doors.add(new Door(true));
         doors.add(new Door(false));
         doors.add(new Door(false));
     }
 
-    @Test
-    void checkNotRiskWin () {
-        //given
-        Player player = new Player("Игрок", false);
-        Game game = new Game(player, doors);
-        //when
-        Door door = game.round(0);
-        //then
-        Assertions.assertTrue(door.isPrize());
+    private static Stream<Arguments> roundArgs() {
+        return Stream.of(
+                Arguments.of(new Player("1", true)
+                        , 0
+                        , false),
+                Arguments.of(new Player("1", true)
+                        , 1
+                        , true),
+                Arguments.of(new Player("1", true)
+                        , 2
+                        , true),
+                Arguments.of(new Player("1", false)
+                        , 0
+                        , true),
+                Arguments.of(new Player("1", false)
+                        , 1
+                        , false),
+                Arguments.of(new Player("1", false)
+                        , 2
+                        , false)
+        );
     }
 
-    @Test
-    void checkNotRiskLose() {
-        //given
-        Player player = new Player("Игрок", false);
+    @ParameterizedTest
+    @MethodSource("roundArgs")
+    void round(Player player, int door, boolean expectedResult) {
         Game game = new Game(player, doors);
-        //when
-        Door door = game.round(1);
-        //then
-        Assertions.assertFalse(door.isPrize());
-    }
-
-    @Test
-    void checkRiskWin () {
-        //given
-        Player player = new Player("Игрок", true);
-        Game game = new Game(player, doors);
-        //when
-        Door door = game.round(1);
-        //then
-        Assertions.assertTrue(door.isPrize());
-    }
-
-    @Test
-    void checkRiskLose () {
-        //given
-        Player player = new Player("Игрок", true);
-        Game game = new Game(player, doors);
-        //when
-        Door door = game.round(0);
-        //then
-        Assertions.assertFalse(door.isPrize());
+        assertEquals(expectedResult, game.round(door).isPrize());
     }
 }
